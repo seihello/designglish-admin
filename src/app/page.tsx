808516b283cc29a686483c54d9be33b235264ea0
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-24 19:51:26 -0700

    fix: solve the select box being overlapped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -299,7 +299,7 @@ export default function Home() {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                   {Array.from(categoryOptions.entries()).map(
                     (categoryOption, index) => (
                       <Listbox.Option
@@ -371,7 +371,7 @@ export default function Home() {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                   {Array.from(phaseOptions.entries()).map(
                     (phaseOption, index) => (
                       <Listbox.Option
